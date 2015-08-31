db9d2b2c2d16eb029175ac8b87e54c7c2066ca50
Taylor Lodge <taylor@boost.co.nz>
2015-08-31 23:08:43 +0000

    API Key creation for new users

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -57,6 +57,8 @@ Spork.prefork do
     # the seed, which is printed after each run.
     #     --seed 1234
     config.order = 'random'
+    config.include Devise::TestHelpers, :type => :controller
+    config.extend ControllerMacros, :type => :controller
   end
 end
 
