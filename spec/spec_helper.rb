0e09d42614f91e1cf6d82d654505845f49f3261d
Taylor Lodge <taylor@boost.co.nz>
2015-09-08 00:19:25 +0000

    Setup CodeClimate test coverage reporting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,7 +25,9 @@ Spork.prefork do
   require 'rspec/rails'
   require 'webmock/rspec'
   # require 'rspec/autorun'
-
+  
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
   Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
