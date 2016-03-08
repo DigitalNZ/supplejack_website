4894cfa3229c356340d311874bea752eae83c62a
EdwinRozario <edwin@boost.co.nz>
2016-03-08 15:07:10 +1300

    Spec for helper method more_tab_title

--- spec/helpers/records_helper_spec.rb
+++ spec/helpers/records_helper_spec.rb
@@ -8,14 +8,30 @@
 
 RSpec.describe RecordsHelper do
 
-  describe "#more_categories_list" do
+  describe '#more_categories_list' do
     before(:each) do
       @categories = { "Guides & factsheets"=>2058, "Journals"=>41886, "Newspapers"=>34577 }
     end
 
-    it "creates a link for each category " do
+    it 'creates a link for each category' do
       expect(helper.more_categories_list(@categories)).to match(/Guides & factsheets <span class="count">2,058<\/span>/)
     end
+  end
+
+  describe "#more_tab_title" do
+    it 'should return "More" if params has no tabs' do
+      expect(more_tab_title).to eq 'More'
+    end
+
+    it 'should return "More" if tab params is one of the SearchTab default tabs' do
+      allow(controller).to receive(:params) { {tab: 'Videos'} }
+      expect(more_tab_title).to eq 'More'
+    end
+
+    it 'should return "Facet name" if tab params is not one of the SearchTab default tabs' do
+      allow(controller).to receive(:params) { {tab: 'Books'} }
+      expect(more_tab_title).to eq 'Books'
+    end    
   end
 
   # Unsure what all this testing is doing, just going to comment out until I revist since it
