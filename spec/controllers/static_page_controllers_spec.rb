d395f0e1a3a495d0bb8e4d4d5c16e2dc8dfc426d
buyifly <buyifly@live.com>
2016-04-20 14:30:05 +1200

    changed the rspec as in the comment review

--- spec/controllers/static_page_controllers_spec.rb
+++ spec/controllers/static_page_controllers_spec.rb
@@ -1,26 +1,18 @@
 require 'spec_helper'
 
 RSpec.describe StaticPagesController do
-
-  describe "GET 'about'" do
-    it "returns http success" do
-      get :about
-      expect(response.status).to eq 200
-    end
+  it "about page is successful" do
+    get :about
+    expect(response.status).to eq 200
   end
 
-  describe "GET 'contact'" do
-    it "returns http success" do
-      get :contact
-      expect(response.status).to eq 200
-    end
+  it "contact page is successful" do
+    get :contact
+    expect(response.status).to eq 200
   end
 
-  describe "GET 'terms'" do
-    it "returns http success" do
-      get :terms
-      expect(response.status).to eq 200
-    end
+  it "terms page is successful" do
+    get :terms
+    expect(response.status).to eq 200
   end
-
 end
