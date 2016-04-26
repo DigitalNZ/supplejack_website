dd4fedf9b591920df98916fc6d44794eda3b6bb1
Reinkaos <rj.matthews23@gmail.com>
2016-04-26 15:05:07 +1200

    Update eager load

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,9 +1,9 @@
 # The majority of The Supplejack Website code is Crown copyright (C) 2014, New Zealand Government,
-# and is licensed under the GNU General Public License, version 3. Some components are 
-# third party components that are licensed under the MIT license or other terms. 
-# See https://github.com/DigitalNZ/supplejack_website for details. 
-# 
-# Supplejack was created by DigitalNZ at the National Library of NZ and the Department of Internal Affairs. 
+# and is licensed under the GNU General Public License, version 3. Some components are
+# third party components that are licensed under the MIT license or other terms.
+# See https://github.com/DigitalNZ/supplejack_website for details.
+#
+# Supplejack was created by DigitalNZ at the National Library of NZ and the Department of Internal Affairs.
 # http://digitalnz.org/supplejack
 
 Demo::Application.configure do
@@ -67,6 +67,7 @@ Demo::Application.configure do
 
   config.action_mailer.default_url_options = { :host => 'beta.dnz0a.digitalnz.org' }
 
+  config.eager_load = true
 
   # Enable threaded mode
   # config.threadsafe!
