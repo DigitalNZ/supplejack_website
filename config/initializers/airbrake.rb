76490da5d346177cfd8ade6724b6182190c81468
Reinkaos <rj.matthews23@gmail.com>
2016-05-24 09:47:18 +1200

    Update airbrake to log when the env variable has not been set

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,5 +1,9 @@
-Airbrake.configure do |config|
-  config.project_key = ENV['AIRBRAKE_PROJECT_API_KEY']
-  config.project_id = ENV['AIRBRAKE_PROJECT_ID']
-  config.ignore_environments = %w(development test)
+if ENV['AIRBRAKE_PROJECT_ID']
+  Airbrake.configure do |config|
+    config.project_key = ENV['AIRBRAKE_PROJECT_API_KEY']
+    config.project_id = ENV['AIRBRAKE_PROJECT_ID']
+    config.ignore_environments = %w(development test)
+  end
+else
+  Rails.logger.debug 'The AIRBRAKE_PROJECT_ID ENV Variable has not been set'
 end
