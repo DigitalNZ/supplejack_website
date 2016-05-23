05235aa4d99d8e96b19060c6aad2b11e657917a5
Taylor Lodge <taylor@boost.co.nz>
2016-05-23 16:13:08 +1200

    Set config_folder for production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,5 +7,7 @@ set :branch, 'master'
 set :deploy_to, '/data/sites/beta.digitalnz.org/rails'
 set :httpdocs, '/data/sites/beta.digitalnz.org/httpdocs'
 
+set :config_folder, '/data/sites/beta.digitalnz.org/conf'
+
 before 'deploy:finished', 'deploy:payload'
 before 'deploy:starting', 'deploy:validate_deployment_description'
