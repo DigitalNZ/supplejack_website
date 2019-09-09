7378b21a52f20627457d81b93fe00346788756f5
Paul Mesnilgrente <info@digitalnz.org>
2019-09-09 15:46:00 +1200

    jquery_ujs is not needed as rails_ujs is already included

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,10 @@
 /*
  * The majority of The Supplejack Website code is Crown copyright (C) 2014, New Zealand Government,
- * and is licensed under the GNU General Public License, version 3. Some components are 
- * third party components that are licensed under the MIT license or other terms. 
- * See https://github.com/DigitalNZ/supplejack_website for details. 
- * 
- * Supplejack was created by DigitalNZ at the National Library of NZ and the Department of Internal Affairs. 
+ * and is licensed under the GNU General Public License, version 3. Some components are
+ * third party components that are licensed under the MIT license or other terms.
+ * See https://github.com/DigitalNZ/supplejack_website for details.
+ *
+ * Supplejack was created by DigitalNZ at the National Library of NZ and the Department of Internal Affairs.
  * http://digitalnz.org/supplejack
  */
 
@@ -21,7 +21,6 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery
-//= require jquery_ujs
 //= require jquery.masonry.min
 //= require jquery.infinitescroll
 //= require foundation
