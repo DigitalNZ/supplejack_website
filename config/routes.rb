66bf4a0c5812b1c2c0380a82dddfa3c495b360ac
Paul Mesnilgrente <info@digitalnz.org>
2019-09-09 17:11:31 +1200

    the find in the supplejack_client has changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,10 @@
-# The majority of The Supplejack Website code is Crown copyright (C) 2014, New Zealand Government,
-# and is licensed under the GNU General Public License, version 3. Some components are
-# third party components that are licensed under the MIT license or other terms.
-# See https://github.com/DigitalNZ/supplejack_website for details.
-#
-# Supplejack was created by DigitalNZ at the National Library of NZ and the Department of Internal Affairs.
-# http://digitalnz.org/supplejack
-
 Rails.application.routes.draw do
   devise_for :users
 
   root to: 'records#home'
-  resources :records,   only: [:index, :show]
+
+  resources :records, only: [:index, :show]
+
   resources :user_sets, only: [:show, :create, :index] do
     resources :set_items, only: [:create]
   end
