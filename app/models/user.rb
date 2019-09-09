c28ab1a8e6dbb8980f3468ffca642be3cd9a4f8e
Paul Mesnilgrente <info@digitalnz.org>
2019-09-09 16:51:31 +1200

    creating the supplejack user after_create is a better practice from overriding the registrations controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,26 @@
 class User < ApplicationRecord
+  include ApiUser
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  after_create :create_supplejack_user
+
+  def create_supplejack_user
+    user = Supplejack::User.create(
+      email: self.email,
+      name: self.name,
+      username: self.username,
+      encrypted_password: self.encrypted_password,
+      sets: [{ name: "Favourites", privacy: "hidden", priority: 0 }]
+    )
+    self.create_key(token: user.api_key, terms: false)
+  rescue StandardError => e
+    byebug
+    Rails.logger.warn("There was a error when creating the API User and default set for member: #{self.id}. Error: #{e.message}")
+    self.errors.add(:base, I18n.t('registration.account_creation_error'))
+    raise ActiveRecord::Rollback
+  end
 end
