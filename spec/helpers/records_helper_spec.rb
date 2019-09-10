8d7c38c0e2bb6913a4d3311fc3eb5173d4805793
Paul Mesnilgrente <info@digitalnz.org>
2019-09-10 15:56:47 +1200

    transpec translated rspec2 syntax to rspec3

--- spec/helpers/records_helper_spec.rb
+++ spec/helpers/records_helper_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe RecordsHelper do
 
     it 'displays the contributing content partner' do
       record = mock_record(source_contributor_name: ['Culture', 'Europeana'])
-      helper.source_contributor_name(record).should eq(%{<p>Culture, Europeana</p>})
+      expect(helper.source_contributor_name(record)).to eq(%{<p>Culture, Europeana</p>})
     end
   end
 
@@ -22,7 +22,7 @@ RSpec.describe RecordsHelper do
     context 'the record has an image' do
       it 'should render a link with an image' do
         record = mock_record(:image_url => 'http://google.com/icon.gif', id: 1, title: 'title')
-        helper.record_thumbnail(record, @search).should eq(%{<div>#{link_to image_tag("http://google.com/icon.gif", :alt => "title", :title => "title"), "/records/1"}</div>})
+        expect(helper.record_thumbnail(record, @search)).to eq(%{<div>#{link_to image_tag("http://google.com/icon.gif", :alt => "title", :title => "title"), "/records/1"}</div>})
       end
     end
 
@@ -33,18 +33,18 @@ RSpec.describe RecordsHelper do
 
     it 'should append the search options to the image link' do
       record = mock_record(:image_url => 'http://google.com/icon.gif', :id => 1)
-      @search.stub(:options) { {:text=>'fish'} }
+      allow(@search).to receive(:options) { {:text=>'fish'} }
 
-      helper.record_thumbnail(record, @search).should match('search%5Btext%5D=fish')
+      expect(helper.record_thumbnail(record, @search)).to match('search%5Btext%5D=fish')
     end
 
     context 'placeholder image' do
       before(:each) do
-        @record = mock(:record)
-        @record.stub(:thumbnail_height) { }
-        @record.stub(:image_url) { }
-        @record.stub(:text?) { false }
-        @record.stub(:creator) { 'Dave' }
+        @record = double(:record)
+        allow(@record).to receive(:thumbnail_height) { }
+        allow(@record).to receive(:image_url) { }
+        allow(@record).to receive(:text?) { false }
+        allow(@record).to receive(:creator) { 'Dave' }
       end
 
     end
@@ -52,24 +52,24 @@ RSpec.describe RecordsHelper do
     it 'should generate a link with no query parameters when there are no search options' do
       record = mock_record(:id => 1)
 
-      @search.stub(:options) { {} }
-      helper.record_thumbnail(record, @search).should match('<a href="\/records\/1">')
+      allow(@search).to receive(:options) { {} }
+      expect(helper.record_thumbnail(record, @search)).to match('<a href="\/records\/1">')
     end
 
     it 'should generate a link with search parameters when search options are passed' do
       record = mock_record(:id => 1)
 
-      @search.stub(:options) { {text: 'fish'} }
-      helper.record_thumbnail(record, @search).should match('<a href="\/records\/1\?search%5Btext%5D=fish">')
+      allow(@search).to receive(:options) { {text: 'fish'} }
+      expect(helper.record_thumbnail(record, @search)).to match('<a href="\/records\/1\?search%5Btext%5D=fish">')
     end
 
     context '#date_parser_for' do
       it "should return the same string if its not a date" do
-        helper.date_parser_for("not a date string").should eq "not a date string"
+        expect(helper.date_parser_for("not a date string")).to eq "not a date string"
       end
 
       it "should return date formated string" do
-        helper.date_parser_for("05-02-1986").should eq "05 Feb 1986"
+        expect(helper.date_parser_for("05-02-1986")).to eq "05 Feb 1986"
       end
     end
 
