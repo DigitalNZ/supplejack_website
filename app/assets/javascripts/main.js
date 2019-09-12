b692876f3e81705d94a128c1efaa95a320983f0a
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2019-09-12 15:27:28 +1200

    upgrade foundation from 5.4 to 6.5

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,65 +1,63 @@
 (function() {
-   'use strict';
+  'use strict';
 
-   var Module =  {};
+  var Module = {};
 
-   Module = (function() {
-      var config = {
-         initMasonry: (function() {
-            var container = $('.gallery-container');
-            container.masonry({
-               itemSelector: '.gallery-record'
-               ,isFitWidth: true
+  Module = (function() {
+    var config = {
+      initMasonry: (function() {
+        var container = $('.gallery-container');
+        container.masonry({
+          itemSelector: '.gallery-record',
+          isFitWidth: true
+        });
+
+        container.imagesLoaded(function() {
+          container.masonry('reload');
+        });
+      }),
+      initInfiniteScroll: (function() {
+        var galleryContainer = $('.gallery-container'),
+          notIE = ($('.ie8').length == 0 && $('.ie7').length == 0);
+
+        if (notIE) {
+          var callback = (function(elements) {
+            var newElements = $(elements),
+              spinner = $('#second-spinner').show();
+
+            galleryContainer.masonry('appended', newElements, true);
+
+            newElements.imagesLoaded(function() {
+              // show elems now they're ready
+              newElements.animate({
+                opacity: 1
+              });
+              spinner.hide();
+
+              newElements.each(function(index, elem) {
+                $(elem).find('a.unknown-height').removeClass('unknown-height');
+              });
+
+              galleryContainer.masonry('reload');
             });
+          });
 
-            container.imagesLoaded(function() {
-               container.masonry('reload');
-            });
-         })
-         ,initInfiniteScroll: (function() {
-            var galleryContainer = $('.gallery-container'),
-                notIE = ($('.ie8').length == 0 && $('.ie7').length == 0);
-
-            if (notIE) {
-               var callback = (function(elements) {
-                  var newElements = $(elements),
-                      spinner = $('#second-spinner').show();
-
-                  galleryContainer.masonry('appended', newElements, true);
-
-                  newElements.imagesLoaded(function(){
-                     // show elems now they're ready
-                     newElements.animate({ opacity: 1 });
-                     spinner.hide();
-
-                     newElements.each(function(index, elem) {
-                        $(elem).find('a.unknown-height').removeClass('unknown-height');
-                     });
-
-                     galleryContainer.masonry('reload');
-                  });
-               });
-
-               galleryContainer.infinitescroll({
-                  navSelector: '#navigation'                 // selector for the paged navigation
-                  ,nextSelector: '#navigation a[rel=next]'   // selector for the NEXT link (to page 2)
-                  ,itemSelector: '.gallery-record'           // selector for all items you'll retrieve
-                  ,loading: {
-                     speed: 'fast'
-                     ,msg: $('<div id="infscr-loading" class="infinite-spinner"><img src="/assets/spinner.gif"></img></div>')
-                  }
-               }, callback);
+          galleryContainer.infinitescroll({
+            navSelector: '#navigation', // selector for the paged navigation
+            nextSelector: '#navigation a[rel=next]', // selector for the NEXT link (to page 2)
+            itemSelector: '.gallery-record', // selector for all items you'll retrieve
+            loading: {
+              speed: 'fast',
+              msg: $('<div id="infscr-loading" class="infinite-spinner"><img src="/assets/spinner.gif"></img></div>')
             }
-         })
-      };
+          }, callback);
+        }
+      })
+    };
 
-      return config;
-   })();
+    return config;
+  })();
 
-   Module.initMasonry();
-   Module.initInfiniteScroll();
+  Module.initMasonry();
+  Module.initInfiniteScroll();
 }());
-
-
-
-
--- app/assets/javascripts/temp.js
+++ app/assets/javascripts/temp.js
@@ -1,10 +1,4 @@
 $('.toggle-add-panel').click(function() {
-   $(this).parent('.add-panel').find('.add-panel-inset').slideToggle();
-   $(this).parent('.add-panel').toggleClass('add-panel-active');
-});
-
-
-//
-$(document).ready(function(){
-   var editor = new MediumEditor('.editable');
+  $(this).parent('.add-panel').find('.add-panel-inset').slideToggle();
+  $(this).parent('.add-panel').toggleClass('add-panel-active');
 });
