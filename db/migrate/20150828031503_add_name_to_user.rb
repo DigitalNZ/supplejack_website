5ae96eaba7406fbbefce34bc761e8c7219794e35
Edwin Rozario <edwin@boost.co.nz>
2021-06-15 14:56:12 +1200

    fix(migrations): migrations have ActiveRecord::Migration version

--- db/migrate/20150828031503_add_name_to_user.rb
+++ db/migrate/20150828031503_add_name_to_user.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddNameToUser < ActiveRecord::Migration
+class AddNameToUser < ActiveRecord::Migration[4.2]
   def change
     add_column :users, :name, :string
   end
--- db/schema.rb
+++ db/schema.rb
@@ -1,48 +1,42 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_150_831_224_313) do
-  create_table 'api_keys', force: true do |t|
-    t.integer  'user_id'
-    t.string   'token'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.boolean  'terms', default: true
+ActiveRecord::Schema.define(version: 2015_08_31_224313) do
+
+  create_table "api_keys", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "token"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean "terms", default: true
   end
 
-  create_table 'records', force: true do |t|
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
+  create_table "records", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table 'users', force: true do |t|
-    t.string   'email',                  default: '', null: false
-    t.string   'encrypted_password',     default: '', null: false
-    t.string   'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.integer  'sign_in_count', default: 0, null: false
-    t.datetime 'current_sign_in_at'
-    t.datetime 'last_sign_in_at'
-    t.string   'current_sign_in_ip'
-    t.string   'last_sign_in_ip'
-    t.datetime 'created_at',                          null: false
-    t.datetime 'updated_at',                          null: false
-    t.string   'name'
-    t.string   'username'
+  create_table "users", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "username"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_index 'users', ['email'], name: 'index_users_on_email', unique: true, using: :btree
-  add_index 'users', ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true, using: :btree
 end
